2ad1e720a0566409e5b95c3b2a319734fcf4ebd8
yeahsilver <gjdpdms2005@gmail.com>
2021-12-08 16:03:18 +0900

    refactor: refactor api key

--- Bodymood/Delegate/AppDelegate.swift
+++ Bodymood/Delegate/AppDelegate.swift
@@ -18,21 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-//    private var kakaoAPIKey: String {
-//        get {
-//            guard let filePath = Bundle.main.path(forResource: "BodymoodInfo", ofType: "plist") else {
-//                fatalError("파일 경로가 존재하지 않습니다.")
-//            }
-//
-//            let plist = NSDictionary(contentsOfFile: filePath)
-//
-//            guard let value = plist?.object(forKey: "KAKAO_API_KEY") as? String else {
-//                fatalError("카카오 API Key를 불러오지 못하였습니다.")
-//            }
-//            return value
-//        }
-//    }
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
         let kakaoAPIKey = Bundle.main.kakaoAPIKey
